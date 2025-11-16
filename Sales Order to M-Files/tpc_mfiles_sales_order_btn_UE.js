e7e137142905be04e7b29c7e9fd0678a55e3e936
shahmeer khan <101963974+shahmeer07@users.noreply.github.com>
2025-11-16 22:21:06 +0000

    Remove credentials from M-Files authentication payload
    Removed sensitive information from payload in the script.

--- Sales Order to M-Files/tpc_mfiles_sales_order_btn_UE.js
+++ Sales Order to M-Files/tpc_mfiles_sales_order_btn_UE.js
@@ -170,10 +170,10 @@ define(["N/https","N/log","N/record","N/ui/serverWidget"], function (https, log,
             log.debug("Sales Order Name: ", salesOrderName);
 
             const payload = {
-                Username: 'NS-MFiles',
-                Password: 'CleanAirRocks!',
-                VaultGuid: '{05931937-E22E-488F-BDC9-F4EE366370F4}',
-                Expiration: "2027-09-30T10:36:29Z"
+                Username: '',
+                Password: '',
+                VaultGuid: '{}',
+                Expiration: ""
             };
 
             const tokenResponse = https.post({
